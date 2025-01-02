c24e011fcba0f60c52fdda7b8e593588d2fa9efd
Ankit Kumar <132771555+Spider1109@users.noreply.github.com>
2025-01-02 12:51:28 +0530

    Add files via upload

---
+++ conditionalStatement.java
@@ -0,0 +1,49 @@
+
+// class Main {
+// static String evenOrOdd(int num) {
+// if (num % 2 == 0) {
+// return "Even"; // Return "Even" if the number is even
+// } else {
+// return "Odd"; // Return "Odd" if the number is odd
+// }
+// }
+
+// public static void main(String[] args) {
+// int num = 5;
+// System.out.println(evenOrOdd(num)); // Print the result of the method
+// }
+// }
+// problem statement2: Take name as input and print a greeting message for that
+// particular name
+// class Main {
+// static String greet(String name) {
+// return "Hello " + name; // Add a space between "Hello" and the name
+// }
+
+// public static void main(String[] args) {
+// String name = "Ankit"; // Enclose the name in double quotes
+// String result = greet(name);
+// System.out.println(result); // Print the greeting
+// }
+// }
+
+// problem statement3: Write a program to input principal, time, and rate (P, T,
+// R) from the user and find Simple Interest.
+
+// class Main {
+// static int simpleInterest(int principal, int rate, int time) {
+// if (principal > 0 && rate > 0 && time > 0) {
+// return (principal * rate * time) / 100; // Calculate interest
+// } else {
+// return 0; // Return 0 if any value is non-positive
+// }
+// }
+
+// public static void main(String[] args) {
+// int p = 1000;
+// int r = 5;
+// int t = 3;
+// int result = simpleInterest(p, r, t);
+// System.out.println("Simple Interest: " + result);
+// }
+// }
